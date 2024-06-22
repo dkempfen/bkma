613636dcdbf403019c53a479583f5b93e400aaa0
dkempfen <d.kempfen@gmail.com>
2024-06-22 19:51:40 -0300

    Mensaje de commit

---
+++ Includes/footer.php
@@ -0,0 +1,25 @@
+    <!-- Essential javascripts for application to work-->
+    <script src="/js/jquery-3.3.1.min.js"></script>
+    <script src="/js/popper.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/main.js"></script>
+    <!-- The javascript plugin to display page loading on top-->
+    <script src="/js/plugins/pace.min.js"></script>
+    <script src="/js/fontawesome.js"></script>
+
+    <script type="text/javascript" src="/js/plugins/sweetalert.min.js"></script>
+    <script type="text/javascript" src="/vali-admin-master/docs/js/plugins/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="/vali-admin-master/docs/js/plugins/dataTables.bootstrap.min.js"></script>
+    <script type="text/javascript" src="/Adman/js/funtion-usuarios.js"></script>
+    <script type="text/javascript" src="/Adman/js/funtions_profesor.js"></script>
+    <script type="text/javascript" src="/Adman/js/funtion-alumno.js"></script>
+    <script type="text/javascript" src="/Adman/js/funtion-carrera.js"></script>
+    <script type="text/javascript" src="/Adman/js/funtion-materia.js"></script>
+
+
+
+
+
+
+  </body>
+</html>
